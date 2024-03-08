b1fb5a1b46db892f5575f9e29aedb72927dea152
Renata Pulz <renata.pulz@raizen.com>
2024-03-08 08:45:05 -0300

    adicionado um evento no botão comprar, para que cada vez que o usuário clique em salvar seja adicionado o nome do produto e valor num array.

--- atividade1/consulta-valor.js
+++ atividade1/consulta-valor.js
@@ -2,12 +2,22 @@ document.addEventListener('DOMContentLoaded', function() {
     let produto = document.getElementById('input-produto');
     let consultaPreco = document.getElementById('botao-consulta');
     let resultado = document.getElementById('reposta');
+    let comprar = document.getElementById('botao-compra');
+    let carrinho = [];
 
     consultaPreco.addEventListener('click', function() {
         let produtoDigitado = produto.value;
-        let valor = consultaDePreco(produtoDigitado);
-        resultado.innerText = 'O valor do produto ' + produtoDigitado + ' é R$ ' + valor.toFixed(2);
+        let valor = consultaDePreco(produtoDigitado).toFixed(2);
+        resultado.innerText = 'O valor do produto ' + produtoDigitado + ' é R$ ' + valor;
     });
+
+    comprar.addEventListener('click', function() {
+        let produtoDigitado = produto.value;
+        let valor = consultaDePreco(produtoDigitado).toFixed(2);
+        carrinho.push({ produto: produtoDigitado, valor: valor });
+        console.log(carrinho);
+    });
+
 });
 
 function consultaDePreco(produtoDigitado) {
