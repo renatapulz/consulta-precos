c416af2c8f3b5c19f7e45e06ee67976f94279342
Renata Pulz <renata.pulz@raizen.com>
2024-03-09 13:49:43 -0300

    Adicionado um botão pra consulta do valor total, com tratativa para o caso de o carrinho estar vazio

--- atividade1/consulta-valor.js
+++ atividade1/consulta-valor.js
@@ -4,18 +4,50 @@ document.addEventListener('DOMContentLoaded', function() {
     let resultado = document.getElementById('reposta');
     let comprar = document.getElementById('botao-compra');
     let carrinho = [];
+    let observacao = document.getElementById('campo-obs');
+    let consulta = document.getElementById('consulta-total');
 
     consultaPreco.addEventListener('click', function() {
         let produtoDigitado = produto.value;
-        let valor = consultaDePreco(produtoDigitado).toFixed(2);
-        resultado.innerText = 'O valor do produto ' + produtoDigitado + ' é R$ ' + valor;
+        if (produtoDigitado !== '') {
+            let produtoDigitado = produto.value;
+            let valor = consultaDePreco(produtoDigitado).toFixed(2);
+            resultado.innerText = 'O valor do produto ' + produtoDigitado + ' é R$ ' + valor;
+            observacao.innerText = ' ';
+        }
+        else {
+            observacao.innerText = 'Insira o código ou nome do produto, por favor.';
+            resultado.innerText = ' ';
+        }
     });
 
     comprar.addEventListener('click', function() {
         let produtoDigitado = produto.value;
-        let valor = consultaDePreco(produtoDigitado).toFixed(2);
-        carrinho.push({ produto: produtoDigitado, valor: valor });
-        console.log(carrinho);
+        let valorRecebido = resultado.innerText.split('R$ ')[1];
+        if (produtoDigitado !== '' && valorRecebido !== undefined) {
+            carrinho.push({ produto: produtoDigitado, valor: valorRecebido });
+            observacao.innerText = 'Produto adicionado ao carrinho!';
+            resultado.innerText = ' ';
+        }
+        else {
+            observacao.innerText = 'Insira o código ou nome do produto, em seguida, consulte o valor.';
+        }
+    });
+
+    consulta.addEventListener('click', function() {
+        let soma = 0;
+        if (carrinho.length > 0) {
+            carrinho.forEach(item => {
+                soma += parseFloat(item.valor);
+            });
+            observacao.innerText = 'Total dos itens somam R$ ' + soma.toFixed(2);
+            
+            .innerText = ' ';
+        }
+        else {
+            observacao.innerText = 'Carrinho vazio';
+            resultado.innerText = ' ';
+        }  
     });
 
 });
